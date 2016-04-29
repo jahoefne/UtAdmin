0f62b82cb87d89cba6f6387187c4009d88b73a4a
jahoefne <jahoefne@gmail.com>
2016-04-29 17:22:12 +0200

    color ranks

--- app/views/RankFormatter.scala
+++ app/views/RankFormatter.scala
@@ -6,17 +6,32 @@ import play.twirl.api.Html
 object RankFormatter {
   def labelForRank(rank: Group) = Html{
     rank match {
+      case x if x.level >= 90 => {
+        """<span class="label label-god">"""  + rank.name + "</span>"
+      }
       case x if x.level >= 80 => {
-        """<span class="label label-success">"""  + rank.name + "</span>"
+        """<span class="label label-headadmin">"""  + rank.name + "</span>"
+      }
+      case x if x.level >= 60 => {
+        """<span class="label label-admin">"""  + rank.name + "</span>"
       }
       case x if x.level >= 55 => {
-        """<span class="label label-primary">"""  + rank.name + "</span>"
+        """<span class="label label-mod">"""  + rank.name + "</span>"
+      }
+      case x if x.level >= 50 => {
+        """<span class="label label-member">"""  + rank.name + "</span>"
       }
       case x if x.level >= 40 => {
-        """<span class="label label-warning">"""  + rank.name + "</span>"
+        """<span class="label label-recruit">"""  + rank.name + "</span>"
+      }
+      case x if x.level >= 30 => {
+        """<span class="label label-friend">"""  + rank.name + "</span>"
+      }
+      case x if x.level >= 20 => {
+        """<span class="label label-regular">"""  + rank.name + "</span>"
       }
       case _ => {
-        """<span class="label label-default">"""  + rank.name + "</span>"
+        """<span class="label label-guest">"""  + rank.name + "</span>"
       }
     }
   }
